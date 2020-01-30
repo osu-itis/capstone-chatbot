c640ef89eef2398f49cc00774979aaf4b3b00d14
Jack Barnes <barnesjt05@gmail.com>
2020-01-30 00:04:23 -0800

    basic cmd structure, relay began, jasmine added

---
+++ chatbot/features/bot_commands.js
@@ -0,0 +1,67 @@
+const request = require('request');
+
+ /*
+    Returns a string with the usage information
+ */
+ function usage_text() {
+    return "Usage: status [vserver || pool] remove [vserver] add [vserver]"
+ }
+/*
+Accepts a string (msg)
+Returns an object with
+ .fulltext = original text
+ .length = number of space-delimited terms
+ .command = first term
+ .target = second term
+ .terms = the remaining terms as an array of strings
+*/
+ function consumeText(msg) {
+    let c_msg = {}
+    c_msg.fulltext = msg;
+    c_msg.terms = msg.split(" ");
+    c_msg.length = c_msg.terms.length;
+    if(c_msg.terms[0]){
+        c_msg.command = c_msg.terms.shift().toLowerCase();
+    }
+    if(c_msg.terms[0]){
+        c_msg.target = c_msg.terms.shift();
+    }
+    return c_msg;
+ }
+
+module.exports = function(controller) {
+    // send welcome/usage
+    controller.on('conversationUpdate', async(bot, message) => {
+        user_name = message.incoming_message.from.name;
+        await bot.reply(message, `Hello ${user_name}.`);
+        await bot.reply(message, `${usage_text()}`);
+    });
+
+    controller.hears(async (message) => message.text, ['message','direct_message'], async (bot, message) => {
+        //parse the message here
+        c_msg = consumeText(message.text);
+        user_name = message.incoming_message.from.name;
+        //the msg must be exactly 2 terms currently
+        if(c_msg.length == 2){
+            //the available commands, currently placeholder replies
+            switch(c_msg.command){
+                case "status":
+                    request(`${process.env.RELAY_URL}`)
+                    await bot.reply(message, `${user_name} wants to know the status of ${c_msg.target}`);
+                    break;
+                case "remove":
+                    await bot.reply(message, `${user_name} wants to remove ${c_msg.target} from the pool`);
+                    break;
+                case "add":
+                    await bot.reply(message, `${user_name} wants to add ${c_msg.target} to the pool`);
+                    break;
+                default:
+                    await bot.reply(message, `Command "${c_msg.command}" not recognized.`);
+                    await bot.reply(message, `${usage_text()}`);
+            }
+        } else {
+            await bot.reply(message, `Command: "${c_msg.fulltext}" not recognized.`);
+            await bot.reply(message, `${usage_text()}`);
+        }
+    });
+}
---
+++ relay/spec/relaySpec.js
@@ -0,0 +1,5 @@
+describe("basicTest", function(){
+    it("Always Passes", function() {
+        expect(true).toBe(true);
+    });
+});
